a1545dbdf6faba8268c0c77284526ad3ffaa5b95
Sayani Koley <sayanikoley8@gmail.com>
2021-08-05 12:51:21 +0530

    Create First Servlet

---
+++ FirstServletProject/src/main/java/com/bridgelabz/servlet/SimpleServlet.java
@@ -0,0 +1,43 @@
+/**
+ * UC1 : Create First Servlet
+ * 
+ * @author SAYANI KOLEY
+ * @since 05.08.2021
+ */
+
+package com.bridgelabz.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Annotation used to declare a servlet. 
+ */
+
+@WebServlet("/SimpleServlet")
+
+/*
+ * Creating a SimpleServlet class
+ * The HttpServlet is an abstract class which contains methods that needs to be overridden by the SimpleServlet class
+ */
+
+public class SimpleServlet extends HttpServlet {
+	
+	/*
+	 * Method to allow a servlet to handle a GET request. 
+	 */
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h3>Hello World Sayani My First Servlet!</h3>");
+		out.close();
+	}
+}
